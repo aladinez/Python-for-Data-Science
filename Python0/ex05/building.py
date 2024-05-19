0cc30495ba60b15fe97ddc93f4f04b8920100ca2
Alae Ezzaouine <52213438+aladinez@users.noreply.github.com>
2024-05-19 16:10:52 +0000

    ex05 in python way, to check ctrl+D

--- Python0/ex05/building.py
+++ Python0/ex05/building.py
@@ -1,56 +1,16 @@
 import sys
 
-def count_uppercase(input):
-    """This function counts the number of uppercase characters in a string."""
-    count = 0
-    for char in input:
-        if char.isupper():
-            count += 1
-    return count
-
-def count_lowercase(input):
-    """This function counts the number of lowercase characters in a string."""
-    count = 0
-    for char in input:
-        if char.islower():
-            count += 1
-    return count
-
-def count_punctuation(input):
-    """This function counts the number of punctuation characters in a string."""
-    punc_chars = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
-    count = 0
-    for char in input:
-        if char in punc_chars:
-            count += 1
-    return count
-
-def count_digits(input):
-    """This function counts the number of digits in a string."""
-    count = 0
-    for char in input:
-        if char.isdigit():
-            count += 1
-    return count
-
-def count_spaces(input):
-    """This function counts the number of spaces in a string."""
-    count = 0
-    for char in input:
-        if char.isspace():
-            count += 1
-    return count
-
-
-
 def analyze_input(input):
     """This function takes a string as input and prints what it contains."""
+
+    punc_chars = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
+
     print("The text contains", len(input), "characters:")
-    print(count_uppercase(input), "upper letters")
-    print(count_lowercase(input), "lower letters")
-    print(count_punctuation(input), "punctuation marks")
-    print(count_spaces(input), "spaces")
-    print(count_digits(input), "digits")
+    print(sum(1 for c in input if c.isupper()), "upper letters")
+    print(sum(1 for c in input if c.islower()), "lower letters")
+    print(sum(1 for c in input if c in punc_chars), "punctuation marks")
+    print(sum(1 for c in input if c.isspace()), "spaces")
+    print(sum(1 for c in input if c.isdigit()), "digits")
 
 
 def main():
