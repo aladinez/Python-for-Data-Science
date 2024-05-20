206d37d1eee77d46fcb031622f0d7675ba7c7b91
Alae Ezzaouine <52213438+aladinez@users.noreply.github.com>
2024-05-20 16:07:27 +0000

    ex08 done.

--- Python0/ex08/Loadin.py
+++ Python0/ex08/Loadin.py
@@ -4,8 +4,7 @@ def ft_tqdm(lst: range) -> None:
     coeff = 100 / total
     for i, elem in enumerate(lst):
         eq = int((i + 1) * coeff)
-        progress_bar = "=" * eq + " " * (100 - eq)
-        print(f"{eq}%|[{progress_bar}]|", end='\r')
-        print(f"{i+1}/{total}", end='\r')
+        progress_bar = "=" * (eq - 1) + ">" + " " * (100 - eq)
+        print(f"{eq}%|[{progress_bar}]|", f"{i+1}/{total}", end='\r')
         yield elem
 
