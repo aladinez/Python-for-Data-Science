6e62febda4bb05bcc8c4cda644f17e1d114ca224
Alae Ezzaouine <52213438+aladinez@users.noreply.github.com>
2024-06-07 14:54:34 +0000

    adjust ex00 test

--- test/Python1_ex00_test.py
+++ test/Python1_ex00_test.py
@@ -2,8 +2,9 @@ import sys
 sys.path.append('/workspaces/Python-for-Data-Science/Python1')
 
 from ex00.give_bmi import give_bmi, apply_limit
-height = [2.71, 1.15]
-weight = [165.3, 38.4]
+
+height = [1.71, 1.65, 1.73, 1.95, 1.63]
+weight = [65.3, 58.4, 63.4, 94.5, 72.9]
 bmi = give_bmi(height, weight)
 print(bmi, type(bmi))
 print(apply_limit(bmi, 26))
